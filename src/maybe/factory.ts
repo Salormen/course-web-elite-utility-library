4653cfadfd634b851f80aef8206386a3fcbb0f53
Juan Acosta <juan.93.ctm@gmail.com>
2024-07-21 20:55:45 -0300

    chore: update factory test

--- src/maybe/factory.ts
+++ src/maybe/factory.ts
@@ -4,7 +4,7 @@ const DEFAULT_VALUE = undefined;
 
 export function buildMaybe<T> (value?: T | Nothing): Maybe<T> {
     if (value === undefined || value === null) {
-        return { value };
+        return { value: DEFAULT_VALUE };
     }
-    return { value: DEFAULT_VALUE };
+    return { value };
 };
